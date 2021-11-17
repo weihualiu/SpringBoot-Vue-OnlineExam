58e6f0198ce015e905c24c57aa062126d0589dd8
liu.weihua <auhiewuil@gmail.com>
2021-11-17 15:58:29 +0800

    debug and run

--- springboot/src/main/java/com/exam/ExamsystemApplication.java
+++ springboot/src/main/java/com/exam/ExamsystemApplication.java
@@ -3,13 +3,20 @@ package com.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication()
-public class ExamsystemApplication {
+@SpringBootApplication
+public class ExamsystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ExamsystemApplication.class, args);
     }
 
+    // 打war包需要重写该方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExamsystemApplication.class);
+    }
 }
 
